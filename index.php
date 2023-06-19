1628f6e130098f788a60ea038cb0b6056f754b09
Skunkie <daannelemans@outlook.com>
2023-06-19 15:14:52 +0200

    updated index

--- index.php
+++ index.php
@@ -8,10 +8,9 @@
 <body>
     <h1>Garage Ertan Admin</h1>
     <p>Please choose an option:</p>
-    <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-    </ul>
+    <a href="login.php">Login</a>
+    <br>
+    <a href="register.php">Register</a>
 </body>
 
 </html>
